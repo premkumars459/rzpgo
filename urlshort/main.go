44c233ae54b848f969e22822bf2b39713217c0f2
premkumars459 <55206728+premkumars459@users.noreply.github.com>
2020-08-07 12:43:07 +0530

    Add files via upload

---
+++ urlshort/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"io/ioutil"
+	"urlshort"
+
+)
+
+func main() {
+	mux := defaultMux()
+
+	// Build the MapHandler using the mux as the fallback
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+
+	// Build the YAMLHandler using the mapHandler as the
+	// fallback
+	yaml := `
+  - path: /urlshort
+    url: https://github.com/gophercises/urlshort
+  - path: /urlshort-final
+    url: https://github.com/gophercises/urlshort/tree/solution
+  `	
+	yhandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	if err != nil {
+		panic (err)
+	}
+
+	f, _ := ioutil.ReadFile("example.json")
+	
+	jhandler , err := urlshort.JsonHandler ([]byte(f),yhandler)
+	if err != nil {
+		panic (err)
+	}
+	fmt.Println("Starting the server on 8080")
+	http.ListenAndServe(":8080" , jhandler)
+	
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
+}
